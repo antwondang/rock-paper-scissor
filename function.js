c7d55f59b0496919e4edb68bb137a55c9ebc83dc
Anthony Dang <anthonydang1999@gmail.com>
2023-09-14 18:27:56 -0700

    Create functions in function.js and link function.js to index.html

--- function.js
+++ function.js
@@ -1,1 +1,30 @@
+// let playerSelection = prompt("Rock, paper or scissor?:");
 
+
+
+function getComputerChoice(){
+    const randomNum = Math.floor(Math.random() * 3);
+
+    switch (randomNum){
+        case 0:
+            return 'Rock'; 
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+        default:
+            return 'Invalid Choice';
+    }
+}
+// const computerChoice = getComputerChoice();
+// console.log('Computer chose: ' + computerChoice);
+
+function pogRound(playerSelection, computerSelection){
+    if (playerSelection == 'Rock' && computerSelection == 'Paper'){
+        return 'You Lose! Paper beats Rock';
+    } 
+}
+
+function game(){
+    
+}
