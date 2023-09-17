4d4ffb75a7ecff0e6263d6c64653ff7e7c940d13
Anthony Dang <anthonydang1999@gmail.com>
2023-09-16 19:10:51 -0700

    Create rock paper scissor game in function.js

--- function.js
+++ function.js
@@ -19,12 +19,27 @@ function getComputerChoice(){
 // const computerChoice = getComputerChoice();
 // console.log('Computer chose: ' + computerChoice);
 
-function pogRound(playerSelection, computerSelection){
-    if (playerSelection == 'Rock' && computerSelection == 'Paper'){
-        return 'You Lose! Paper beats Rock';
-    } 
-}
+function pogRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+  
+    if (playerSelection === 'rock' && computerSelection === 'Paper') {
+      return 'You Lose! Paper beats Rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+      return 'You Win! Paper beats Rock';
+    } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+      return 'You Win! Scissors beats Paper';
+    } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+      return 'You Win! Rock beats Scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+      return 'You Lose! Scissors beats Paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+      return 'You Lose! Rock beats Scissors';
+    } else {
+      return 'It\'s a tie!';
+    }
+  }
 
-function game(){
-    
-}
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+console.log(pogRound(playerSelection, computerSelection));
+
